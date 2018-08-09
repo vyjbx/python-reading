6a3a008d94d297c400cbaf3d755366a48b8c00ac
Dahu Qi <dahu@moven.com>
2018-08-09 18:33:14 -0400

    Revert "testing commit"
    revert the test commit to one commit earlier.
    
    This reverts commit 62719029e08c0eb918329dabbd259a9136a39d2a.

--- python-scripts/search-memorization.py
+++
@@ -1,8 +0,0 @@
-'''
-We have a recent problem to find the best solution from a range of numbers. The function will be called millions of times. The numbers are integers in a range defined by upper_limit and lower_limit. The number of choices is not infinite.
-There are few ways to search for the best solution.
-1. brute-force sequential search, maybe with some parallization (unfortunately python only benefits in I/O heavy applicatons.)
-2. binary search, and when it may not work
-3. prioritized queue
-4. record conditions and memorization 
-'''
